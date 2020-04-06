77ed9c869b3b9d26eaea10afb4537c18edb6b872
seedevk8s <seedevk8s@gmail.com>
2020-04-06 10:18:59 +0900

    null checking

--- src/User.js
+++ src/User.js
@@ -3,6 +3,13 @@ import React from 'react';
 /* 이 컴포넌트는 user 라는 props 를 받아와서 
 해당 데이터의 id 와 username 값을 보여줍니다. */
 function User({ user }) {
+    /* 이렇게 하시면 user 값이 존재하지 않는다면 null 을 렌더링하게 됩니다. 
+    리액트 컴포넌트에서 null 을 렌더링하게되면 아무것도 나타나지 않게 됩니다. 
+    이를 "null checking" 이라고 부릅니다. */
+    if (!user) {
+        return null;
+    }
+
 
     return (
         <div>
